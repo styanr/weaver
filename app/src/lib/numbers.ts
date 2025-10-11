f88db89169ca9442c66c94bc68b766595aed24d2
styanr <99873857+styanr@users.noreply.github.com>
2025-10-11 22:48:13 +0300

    fix: fetch correct spells for level 0

--- app/src/lib/numbers.ts
+++ app/src/lib/numbers.ts
@@ -2,49 +2,57 @@
 
 export function romanize(num: number) {
   if (isNaN(num)) return NaN;
-  var digits = String(+num).split(''),
-    key = [
-      '',
-      'C',
-      'CC',
-      'CCC',
-      'CD',
-      'D',
-      'DC',
-      'DCC',
-      'DCCC',
-      'CM',
-      '',
-      'X',
-      'XX',
-      'XXX',
-      'XL',
-      'L',
-      'LX',
-      'LXX',
-      'LXXX',
-      'XC',
-      '',
-      'I',
-      'II',
-      'III',
-      'IV',
-      'V',
-      'VI',
-      'VII',
-      'VIII',
-      'IX',
-    ],
-    roman = '',
-    i = 3;
-  while (i--) roman = (key[+digits.pop() + i * 10] || '') + roman;
+  const digits = String(+num).split('');
+
+  const key = [
+    '',
+    'C',
+    'CC',
+    'CCC',
+    'CD',
+    'D',
+    'DC',
+    'DCC',
+    'DCCC',
+    'CM',
+    '',
+    'X',
+    'XX',
+    'XXX',
+    'XL',
+    'L',
+    'LX',
+    'LXX',
+    'LXXX',
+    'XC',
+    '',
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX',
+  ];
+  let roman = '';
+  let i = 3;
+
+  while (i--) {
+    const digit = digits.pop();
+
+    if (digit) {
+      roman = (key[+digit + i * 10] || '') + roman;
+    }
+  }
   return Array(+digits.join('') + 1).join('M') + roman;
 }
 
 export const parsePositiveIntegerParam = (s: string | null): number | null => {
   if (!s) return null;
   const t = s.trim();
-  if (!/^[1-9]\d*$/.test(t)) return null;
+  if (!/^[0-9]\d*$/.test(t)) return null;
 
   const n = Number(t);
   return Number.isSafeInteger(n) ? n : null;
